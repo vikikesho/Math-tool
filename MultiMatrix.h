76b167af820bb29ff8d05e03dea25c6672dce2ae
vikikesho <55180949+vikikesho@users.noreply.github.com>
2019-11-01 19:26:34 +0700

    Delete MultiMatrix.h

--- MultiMatrix.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<iostream>
-using namespace std;
-void nhap(int a[] , int& m, int& n);
-void xuat(int a[], int m, int n);
-void multiply(int m1, int m2, int a1[], int n1, int n2, int a2[]);
